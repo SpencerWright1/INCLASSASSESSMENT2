be5906ae0a5a124cd01672ded0153b7bd1f9ea64
Spencer Wright <swright41n@gmail.com>
2025-07-10 19:29:19 -0600

    Beginning of Assessment - After loading the starting files into your project

---
+++ Starting Files/eToursWebApp/Model/Excursion.cs
@@ -0,0 +1,71 @@
+﻿namespace eToursWebApp.Model
+{
+    public class Excursion
+    {
+        private string _Name;
+        private double _Duration;
+        public string Name
+        {
+            get { return _Name; }
+            set
+            {
+                if(string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Name", "Name is required. Cannot be empty.");
+                }
+                _Name =value;
+            }
+        }
+
+        public DateTime ExcursionDate { get; set; }
+
+        public double Duration
+        {
+            get { return _Duration; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException($"Duration {value} is invalid. Must be greater than 0.","Duration");
+                }
+                _Duration = value;
+            }
+        }
+
+        public ExcursionType TourType { get; set; }
+
+        public Excursion(string name, DateTime excursiondate, double duration, ExcursionType tourtype)
+        {
+            if (excursiondate < DateTime.Today.AddDays(1))
+            {
+                throw new ArgumentException($"Excursion date {excursiondate} is invalid. Must be a date greater than today", "ExcurationDate");
+            }
+            Name = name;
+            ExcursionDate = excursiondate;
+            Duration = duration;
+            TourType = tourtype;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name},{ExcursionDate.ToString("MMM-dd-yyyy")},{Duration},{TourType}";
+        }
+
+        public static Excursion Parse(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                throw new ArgumentNullException("ParseString", "No data present to Parse.");
+            }
+            string[] items = text.Split(',');
+            if (items.Length != 4)
+            {
+                throw new FormatException($"Data string is invalid. Insufficent/Excessive string values. Data: {text}");
+            }
+            return new Excursion(items[0],
+                                 DateTime.Parse(items[1]),
+                                 double.Parse(items[2]),
+                                 Enum.Parse<ExcursionType>(items[3]));
+        }
+    }
+}
---
+++ Starting Files/eToursWebApp/Model/ExcursionType.cs
@@ -0,0 +1,12 @@
+﻿namespace eToursWebApp.Model
+{
+    public enum ExcursionType
+    {
+        Walk,
+        BusTour,
+        Bike,
+        Water,
+        Dive,
+        Moped
+    }
+}
